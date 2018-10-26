e3505dff0c01ed5726604fc3aaa5e77a34e83f69
Gabriel Augendre <gabriel@augendre.info>
2018-10-26 21:20:12 +0200

    Improve admin for directory entries.

--- portailva/directory/admin.py
+++ portailva/directory/admin.py
@@ -3,5 +3,22 @@ from django.contrib import admin
 # Register your models here.
 from portailva.directory.models import DirectoryEntry, OpeningHour
 
-admin.site.register(DirectoryEntry)
 admin.site.register(OpeningHour)
+
+@admin.register(DirectoryEntry)
+class DirectoryEntryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'asso_name', 'created_at', 'is_online'
+    list_editable = 'is_online',
+    list_display_links = 'id',
+    search_fields = 'association__name',
+    list_filter = 'is_online',
+    date_hierarchy = 'created_at'
+    ordering = 'id',
+
+    def asso_name(self, obj):
+        return obj.association.name
+
+    asso_name.short_description = "Nom de l'association"
+    asso_name.admin_order_field = 'association__name'
+
+
