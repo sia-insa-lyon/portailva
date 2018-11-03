45e5ec150953a07f2638bd7d4eb27b625b465a3a
Gabriel Augendre <gabriel@augendre.info>
2018-11-03 17:39:11 +0100

    Replace backslashes first

--- portailva/directory/templatetags/latex.py
+++ portailva/directory/templatetags/latex.py
@@ -6,10 +6,10 @@ register = template.Library()
 @register.filter
 def escape_latex_specials(value):
     return (value
+            .replace('\\', '\\backslash{}')
             .replace('&', '\\&')
             .replace('#', '\\#')
             .replace('_', '\\_')
-            .replace('\\', '\\backslash{}')
             .replace('€', '\\euro{}')
             .replace('$', '\\$')
             .replace('n°', '\\no')
