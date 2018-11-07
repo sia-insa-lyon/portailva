e7224427caea01e62730bda50838ae189bca3970
Gabriel Augendre <gabriel@augendre.info>
2018-11-07 13:09:20 +0100

    Fix \no replacement

--- portailva/directory/templatetags/latex.py
+++ portailva/directory/templatetags/latex.py
@@ -12,7 +12,7 @@ def escape_latex_specials(value):
             .replace('_', '\\_')
             .replace('€', '\\euro{}')
             .replace('$', '\\$')
-            .replace('n°', '\\no')
+            .replace('n°', '\\no{}')
             .replace('·', '.')
             .replace('\n\n', '\n')
             .replace('\n', '\n\n')
