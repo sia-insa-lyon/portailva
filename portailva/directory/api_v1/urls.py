4ea478ee0e2094523e023a87a7f5a912f028cbea
Halunka Matthieu <matthieu.halunka@laposte.net>
2019-08-21 03:03:35 +0200

    Adding test and end point to get public info of an association

--- portailva/directory/api_v1/urls.py
+++ portailva/directory/api_v1/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import DirectoryAPIView, PrivateDirectoryAPIView
+from .views import DirectoryAPIView, PrivateDirectoryAPIView, PublicDirectoryByIdAPIView
 
 urlpatterns = [
     url('^private/$', PrivateDirectoryAPIView.as_view(), name='api-v1-directory-private'),
+    url('^public/(?P<association_pk>\d+)/$', PublicDirectoryByIdAPIView.as_view(), name='api-v1-directory-public'),
     url('^$', DirectoryAPIView.as_view(), name='api-v1-directory-index'),
 ]
