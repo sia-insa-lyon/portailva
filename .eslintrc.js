12d9b263a4db6c60c2637bbfa1f6ff358e2e96ae
mhalunka <mhalunka@ippon.fr>
2019-08-27 16:46:58 +0200

    Adding linter for JS

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+  },
+  'rules': {
+      'comma-dangle': 'off',
+      'indent':['error', 4],
+      'implicit-arrow-linebreak': 'off',
+      'require-jsdoc': 'off',
+      'max-len': ["error", { "code": 180 }],
+      'no-trailing-spaces': 'off',
+      'semi': ['error','always'],
+  },
+};
