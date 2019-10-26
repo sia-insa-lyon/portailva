eb90a60274ad8aabba5e772cd5ed408c56b200d0
Gabriel Augendre <gabriel@augendre.info>
2019-10-26 16:46:18 +0200

    Improve place admin

--- portailva/utils/admin.py
+++ portailva/utils/admin.py
@@ -1,5 +1,12 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
 
 from portailva.utils.models import Place
 
-admin.site.register(Place)
+
+@register(Place)
+class PlaceAdmin(ModelAdmin):
+    list_display = [
+        'name',
+        'lat',
+        'long',
+    ]
