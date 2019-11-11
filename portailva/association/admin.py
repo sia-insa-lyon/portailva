d054468447e23ab813628a4930f96cfa7b075572
Halunka Matthieu <matthieu.halunka@laposte.net>
2019-11-11 13:25:31 +0000

    FIX #64: Display correctly mandate for admin
    Signed-off-by: Halunka Matthieu <matthieu.halunka@laposte.net>

--- portailva/association/admin.py
+++ portailva/association/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from .models import Category, Association, Mandate, PeopleRole, People, Requirement, Accomplishment
 
 
+def publish_phone(modeladmin, request, queryset):
+    queryset.update(share_phone=True)
+
+
+publish_phone.short_description = "Publier les numéros des mandats sélectionnés"
+
+
+def hide_phone(modeladmin, request, queryset):
+    queryset.update(share_phone=False)
+
+
+hide_phone.short_description = "Cacher les numéros des mandats sélectionnés"
+
+
 @admin.register(Association)
 class AssociationAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'acronym', 'category', 'active_members_number', 'is_active', 'is_validated', 'has_place'
@@ -12,8 +26,16 @@ class AssociationAdmin(admin.ModelAdmin):
     ordering = 'name', 'id'
 
 
+@admin.register(Mandate)
+class AssociationMandateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'association', 'begins_at', 'ends_at', 'share_phone')
+    list_display_links = ('id', 'association')
+    search_fields = ('id', 'association__name')
+    list_per_page = 50
+    actions = [publish_phone, hide_phone]
+
+
 admin.site.register(Category)
-admin.site.register(Mandate)
 admin.site.register(PeopleRole)
 admin.site.register(People)
 admin.site.register(Requirement)
