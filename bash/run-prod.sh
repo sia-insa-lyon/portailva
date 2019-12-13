79a72040ff84369781dd93e8826bf6a86b6278b1
Gabriel Augendre <gabriel@augendre.info>
2019-12-13 16:34:33 +0000

    Replace gunicorn with uWSGI

--- bash/run-prod.sh
+++ bash/run-prod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pipenv run python3 manage.py migrate && pipenv run python3 manage.py collectstatic --noinput && pipenv run gunicorn portailva.wsgi -b 0.0.0.0:8000 --log-file -
+pipenv run python3 manage.py migrate && pipenv run python3 manage.py collectstatic --noinput && pipenv run uwsgi --module=portailva.wsgi:application --http=0.0.0.0:8000 --threads=4 --processes=1
